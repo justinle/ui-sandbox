48e8f0069461f34f021a4443e5e92c65b7ba286f
Justin Le <justinle9@gmail.com>
2024-05-25 11:39:05 -0400

    before ovalish change

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ThinLineToggle from "./components/switch/thinLine/ThinLineToggle.tsx";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <ThinLineToggle />
+      </div>
+    </>
+  )
+}
+
+export default App
